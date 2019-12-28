94914431abfebbcc25850f88ab0cc4da85dcb1a3
Salomon Romano <salo_romano@yahoo.com>
2019-12-28 16:49:19 -0600

    Integracion de jQuery y librerías externas

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-contact',
@@ -10,6 +11,10 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.galeria').bxSlider({
+      mode: 'fade',
+      captions: false,
+      slideWidth: 400
+    });
   }
-
 }
